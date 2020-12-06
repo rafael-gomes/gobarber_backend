42cd339811996ed0a960146fd079f5eacaecf549
Rafael Gomes <rafaell.gomes@gmail.com>
2020-12-05 22:03:50 -0300

    Change type of id for uuid

--- src/shared/infra/typeorm/migrations/1604622095897-CreateAppointments.ts
+++ src/shared/infra/typeorm/migrations/1604622095897-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1604622095897
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
